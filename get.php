aa117649e9017b650295740d477a01baca48be92
j129008 <j129008@gmail.com>
2015-12-26 15:58:58 +0800

    unfinish proximity search

--- get.php
+++ get.php
@@ -67,11 +67,17 @@ if(!empty($_POST)){
         $keywordList = array();
 
         $flag = "key";
+        $slop = "0";
+        $op = "";
+        $i = 0;
         foreach(explode(" ", $keyword) as $token){
             if(strcmp($token, "and") == 0){
                 $flag = "and";
             }else if(strcmp($token, "or") == 0){
                 $flag = "or";
+            }else if(strpos($token, "/") === 0){
+                $flag = "/";
+                $slop = substr($token, 1, strlen($token)-1);
             }else if(strcmp($token, "not") == 0){
                 $flag = "not";
             }else if(strcmp($flag, "and") == 0){
@@ -85,6 +91,8 @@ if(!empty($_POST)){
                     );
                 array_push($params['body']['query']['bool']['must'],$and);
                 array_push($keywordList, $token);
+                $flag = "key";
+                $op = "must";
             }else if(strcmp($flag, "key") == 0){
                 $and = array(
                         'match' => array(
@@ -96,6 +104,7 @@ if(!empty($_POST)){
                     );
                 array_push($params['body']['query']['bool']['must'],$and);
                 array_push($keywordList, $token);
+                $op = "must";
             }else if(strcmp($flag, "or") == 0){
                 $or = array(
                         'match' => array(
@@ -107,6 +116,8 @@ if(!empty($_POST)){
                     );
                 array_push($params['body']['query']['bool']['should'],$or);
                 array_push($keywordList, $token);
+                $flag = "key";
+                $op = "should";
             }else if(strcmp($flag, "not") == 0){
                 $not = array(
                         'match' => array(
@@ -117,7 +128,13 @@ if(!empty($_POST)){
                         )
                     );
                 array_push($params['body']['query']['bool']['must_not'],$not);
+                $flag = "key";
+                $op = "must_not";
+            }else if(strcmp($flag, "/") == 0){
+                array_push($keywordList, $token);
+                $flag = "key";
             }
+            $i = $i + 1 ;
         }
 
         $results = $client->search($params);
